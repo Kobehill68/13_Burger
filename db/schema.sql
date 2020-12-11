045996498deae23fac1e9e2e4c0ef9c7e2532b7e
KobeHill68 <kobehill68@gmail.com>
2020-12-11 10:23:25 -0600

    done making the table

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,4 @@
+DROP DATABASE IF EXISTS burgers_db;
 CREATE DATABASE burgers_db;
 
 USE burgers_db;
@@ -6,6 +7,6 @@ CREATE TABLE burgers
 (
 	id int NOT NULL AUTO_INCREMENT,
 	burger_name varchar(255) NOT NULL,
-    devoured
+    devoured BOOLEAN DEFAULT false
 	PRIMARY KEY (id)
 );
