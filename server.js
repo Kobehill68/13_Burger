2bc301221cd1f2708a159288f412c88ed9e0c95b
KobeHill68 <kobehill68@gmail.com>
2020-12-11 14:18:52 -0600

    made my server

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 3030
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+const exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+const routes = require("./controllers/burgers_controller");
+app.use(routes);
+
+app.listen(PORT, function() {
+    console.log("Server is listening on PORT " + PORT)
+});
