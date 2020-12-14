04821f69cdda73bee8382af4c37a618eca3975eb
KobeHill68 <kobehill68@gmail.com>
2020-12-14 13:31:38 -0600

    changed my model

--- models/burger.js
+++ models/burger.js
@@ -1,5 +1,30 @@
 const orm = require("../config/orm");
 
 const burger = {
-    
+    all: function(cb) {
+        orm.selectAll("burgers", function(res) {
+            cb(res)
+        });
+    },
+
+    create: function(cols, vals, cb) {
+        orm.insertOne("burgers", cols, vals, function(result) {
+            cb(result)
+        });
+    },
+
+    update: function(objColsVals, condition, cb) {
+        orm.updateOne("burgers", objColsVals, condition, (result) => {
+            cb(result)
+        });
+    },
+
+    delete: function(cb) {
+        orm.deleteAll("burgers", (result) => {
+            cb(result)
+        });
+    }
+   
 }
+
+module.exports = burger;
