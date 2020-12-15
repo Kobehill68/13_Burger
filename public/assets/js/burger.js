49fd19d63b1fa676bb41481054387d0e6d06b790
KobeHill68 <kobehill68@gmail.com>
2020-12-14 19:38:09 -0600

    added new file

---
+++ public/assets/js/burger.js
@@ -0,0 +1,39 @@
+$(function () {
+    $(".devour").on("click", function() {
+        const id = $(this).data("id");
+        // const devour = $(this).data("devour");
+    
+        const devouredBurger = {
+          devoured: 1
+        };
+    
+        // Send the PUT request.
+        $.ajax("/api/burgers/" + id, {
+          type: "PUT",
+          data: devouredBurger
+        }).then(() => {          
+            location.reload();
+          }
+        );
+    });
+    
+    $(".create-form").on("submit", function(event) {
+        // Make sure to preventDefault on a submit event.
+        event.preventDefault();  
+        const newBurger = {
+          burger_name: $("#burger").val().trim(),        
+        };
+    
+        // Send the POST request.
+        $.ajax("/api/burgers", {
+          type: "POST",
+          data: newBurger
+        }).then(() => {
+            console.log("created new burger");          
+            location.reload();
+          }
+        );
+    });
+
+
+})
