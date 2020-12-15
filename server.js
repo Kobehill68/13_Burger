092e1af73980be89adf7713f32bebf40bb38172b
KobeHill68 <kobehill68@gmail.com>
2020-12-15 10:08:43 -0600

    new files

--- server.js
+++ server.js
@@ -3,10 +3,9 @@ const app = express();
 
 const PORT = process.env.PORT || 8080
 
-app.use(express.static("public"));
-
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+app.use(express.static("public"));
 
 const exphbs = require("express-handlebars");
 
